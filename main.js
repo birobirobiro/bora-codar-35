62ec2f463a77c876fd10232c0ddd7c42bee1b286
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-09-01 16:42:01 -0300

    refactor

--- main.js
+++ main.js
@@ -1,116 +1,133 @@
-// Obter os elementos de input do tipo range e seus respectivos elementos de output
-const metaDiariaInput = document.getElementById("dailyGoal");
-const metaDiariaOutput = document.getElementById("dailyGoalValue");
-const quantidadeInput = document.getElementById("dailyQuantity");
-const quantidadeOutput = document.getElementById("quantityValue");
+// DOM elements
+const dailyGoalInput = document.getElementById("dailyGoal");
+const dailyGoalOutput = document.getElementById("dailyGoalValue");
+const quantityInput = document.getElementById("dailyQuantity");
+const quantityOutput = document.getElementById("quantityValue");
+const hoursElement = document.querySelector(".countdown .hours span");
+const minutesElement = document.querySelector(".countdown .minutes span");
+const startButton = document.getElementById('start');
+const modalContainer = document.querySelector('.modalContainer');
+const closeButton = document.getElementById('close');
+const percentageDisplay = document.getElementById('percentage');
+const goal = document.getElementById('goal');
 
-let totalGlassesTaken = -1
+
+// Initializations
+let totalGlassesTaken = -1;
+let isCounting = false;
+let timer;
+
+// Constants
+const TIME_AWAKE_IN_MINUTES = 960; // Equivalent to 16 hours awake
+
+// Event listeners
+dailyGoalInput.addEventListener("input", handledailyGoalInput);
+quantityInput.addEventListener("input", handlequantityInput);
+startButton.addEventListener('click', handleStartButtonClick);
+closeButton.addEventListener('click', closeModal);
+
+// Event handlers
+function handledailyGoalInput() {
+  const goalInMilliliters = Number(dailyGoalInput.value);
+  dailyGoalOutput.textContent = `${goalInMilliliters}ml`;
+  const goalInLiters = (goalInMilliliters / 1000).toFixed(1);
+  goal.textContent = `Meta: ${goalInLiters} L`; // Set the goal in liters
+  updateHourAndMinutesDisplay();
+}
+
+
+function handlequantityInput() {
+  quantityOutput.textContent = `${quantityInput.value}ml`;
+  updateHourAndMinutesDisplay();
+}
+
+function handleStartButtonClick() {
+  if (isCounting) return;
+
+  // Disable input type range elements
+  dailyGoalInput.disabled = true;
+  quantityInput.disabled = true;
+
+  // Add the 'disabled-input' class to change cursor style
+  dailyGoalInput.classList.add('disabled-input');
+  quantityInput.classList.add('disabled-input');
+
+  // Change the button text to "Timer Iniciado"
+  startButton.textContent = "Timer Iniciado";
+
+  isCounting = true;
+  countdown();
+}
+
+// Functions
 function calculate() {
-  const TIME_AWAKE_IN_MINUTES = 960 // equivalente a 16h acordado
-  const totalGlassesOfWater = Number(metaDiariaInput.value / quantidadeInput.value)
-  const totalTimerInMinutes = TIME_AWAKE_IN_MINUTES / totalGlassesOfWater
-  const [hour, minutes] = [Math.floor(totalTimerInMinutes / 60), Math.floor(totalTimerInMinutes % 60)]
+  const totalGlassesOfWater = Number(dailyGoalInput.value / quantityInput.value);
+  const totalTimerInMinutes = TIME_AWAKE_IN_MINUTES / totalGlassesOfWater;
+  const [hour, minutes] = [Math.floor(totalTimerInMinutes / 60), Math.floor(totalTimerInMinutes % 60)];
+  const percentageOfTotalGlassesTaken = Math.abs((totalGlassesTaken * 100 / totalGlassesOfWater) - 100) + '%';
 
-  const percentageOfTotalGlassesTaken = 
-    Math.abs((totalGlassesTaken * 100 / totalGlassesOfWater) - 100) + '%'
-  
   return {
     canTakeWater: totalGlassesTaken + 1 <= totalGlassesOfWater,
     hour,
     minutes,
     percentageOfTotalGlassesTaken
-  }  
+  };
 }
-
-// Atualizar o output da meta diária quando o input do tipo range mudar
-metaDiariaInput.addEventListener("input", function () {
-  metaDiariaOutput.textContent = `${metaDiariaInput.value}ml`;
-  updateHourAndMinutesDisplay()
-});
-
-// Atualizar o output da quantidade quando o input do tipo range mudar
-quantidadeInput.addEventListener("input", function () {
-  quantidadeOutput.textContent = `${quantidadeInput.value}ml`;
-  updateHourAndMinutesDisplay()
-});
-
-// Obter os elementos do contador
-
-const horasElemento = document.querySelector(".countdown .hours span");
-const minutosElemento = document.querySelector(".countdown .minutes span");
 
 function updateHourAndMinutesDisplay() {
-  const {hour, minutes} = calculate()
-  horasElemento.textContent = hour
-  minutosElemento.textContent = minutes
+  const { hour, minutes } = calculate();
+  hoursElement.textContent = hour;
+  minutesElement.textContent = minutes;
 }
-
-updateHourAndMinutesDisplay()
-
-// start counter 
-let isCounting = false, timer;
 
 function countdown() {
-  clearInterval(timer)
-  if(!isCounting) return
+  clearInterval(timer);
+  if (!isCounting) return;
 
-  const { canTakeWater } = calculate()
-  if(!canTakeWater) return
-  
-  let newHour = horasElemento.textContent,
-  newMinutes = Number(minutosElemento.textContent) - 1
-  
-  if(newMinutes < 0) {
-    newMinutes = 59
-    newHour = Number(horasElemento.textContent) - 1 
+  const { canTakeWater } = calculate();
+  if (!canTakeWater) return;
+
+  let newHour = hoursElement.textContent;
+  let newMinutes = Number(minutesElement.textContent) - 1;
+
+  if (newMinutes < 0) {
+    newMinutes = 59;
+    newHour = Number(hoursElement.textContent) - 1;
   }
 
-  if(newHour < 0) {
-    clearInterval(timer)
+  if (newHour < 0) {
+    clearInterval(timer);
     isCounting = false;
-    openModal()
-    return
+    openModal();
+    return;
   }
 
-  horasElemento.textContent = newHour
-  minutosElemento.textContent = newMinutes
+  hoursElement.textContent = newHour;
+  minutesElement.textContent = newMinutes;
 
-  timer = setInterval(() => { countdown() }, 10)
-}
-
-const startButton = document.getElementById('start')
-startButton.addEventListener('click', () => {
-  if(isCounting) return
-  
-  isCounting = true;
-  countdown()
-})
-
-
-// Modal
-const modalContainer = document.querySelector('.modalContainer')
-const closeButton = document.getElementById('close')
-
-closeButton.addEventListener('click', closeModal)
-
-function closeModal() {
-  modalContainer.style.display = 'none'
-  isCounting = true
-  updateHourAndMinutesDisplay()
-  countdown()
-  updatePercentageDisplay()
+  timer = setInterval(countdown, 10);
+  // timer = setInterval(countdown, 60000); // Default is 1 minute
 }
 
 function openModal() {
   isCounting = false;
-  modalContainer.style.display = 'block'
+  modalContainer.style.display = 'block';
+}
+
+function closeModal() {
+  modalContainer.style.display = 'none';
+  isCounting = true;
+  updateHourAndMinutesDisplay();
+  countdown();
+  updatePercentageDisplay();
 }
 
 function updatePercentageDisplay() {
-  totalGlassesTaken++
-  const { percentageOfTotalGlassesTaken } = calculate()
-  
-  const percentageDisplay = document.getElementById('percentage')
-  percentageDisplay.textContent = percentageOfTotalGlassesTaken
+  totalGlassesTaken++;
+  const { percentageOfTotalGlassesTaken } = calculate();
+  percentageDisplay.textContent = percentageOfTotalGlassesTaken;
 }
-updatePercentageDisplay()
+
+// Initial updates
+updateHourAndMinutesDisplay();
+updatePercentageDisplay();
