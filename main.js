c9a4fc3f920539949276da9ca3c19bebab9b2d9e
Mayk Brito <maykbrito@gmail.com>
2023-09-01 14:55:06 -0300

    playing around

--- main.js
+++ main.js
@@ -4,76 +4,113 @@ const metaDiariaOutput = document.getElementById("dailyGoalValue");
 const quantidadeInput = document.getElementById("dailyQuantity");
 const quantidadeOutput = document.getElementById("quantityValue");
 
+let totalGlassesTaken = -1
+function calculate() {
+  const TIME_AWAKE_IN_MINUTES = 960 // equivalente a 16h acordado
+  const totalGlassesOfWater = Number(metaDiariaInput.value / quantidadeInput.value)
+  const totalTimerInMinutes = TIME_AWAKE_IN_MINUTES / totalGlassesOfWater
+  const [hour, minutes] = [Math.floor(totalTimerInMinutes / 60), Math.floor(totalTimerInMinutes % 60)]
+
+  const percentageOfTotalGlassesTaken = 
+    Math.abs((totalGlassesTaken * 100 / totalGlassesOfWater) - 100) + '%'
+  
+  return {
+    canTakeWater: totalGlassesTaken + 1 <= totalGlassesOfWater,
+    hour,
+    minutes,
+    percentageOfTotalGlassesTaken
+  }  
+}
+
 // Atualizar o output da meta diária quando o input do tipo range mudar
 metaDiariaInput.addEventListener("input", function () {
   metaDiariaOutput.textContent = `${metaDiariaInput.value}ml`;
+  updateHourAndMinutesDisplay()
 });
 
 // Atualizar o output da quantidade quando o input do tipo range mudar
 quantidadeInput.addEventListener("input", function () {
   quantidadeOutput.textContent = `${quantidadeInput.value}ml`;
+  updateHourAndMinutesDisplay()
 });
 
 // Obter os elementos do contador
+
 const horasElemento = document.querySelector(".countdown .hours span");
 const minutosElemento = document.querySelector(".countdown .minutes span");
 
-// Variáveis para controlar o contador
-let metaDiaria = parseInt(metaDiariaInput.value);
-let quantidade = parseInt(quantidadeInput.value);
-let contador = 0;
-
-// Função para atualizar o contador com base na meta diária e quantidade
-function atualizarContador() {
-  // Calcular o número de minutos para a meta diária
-  const totalMinutos = Math.floor(metaDiaria / quantidade) * 60;
-
-  // Calcular o número de horas e minutos restantes
-  const horasRestantes = Math.floor(totalMinutos / 60);
-  const minutosRestantes = totalMinutos % 60;
-
-  // Atualizar os elementos do contador
-  horasElemento.textContent = horasRestantes.toString().padStart(2, "0");
-  minutosElemento.textContent = minutosRestantes.toString().padStart(2, "0");
+function updateHourAndMinutesDisplay() {
+  const {hour, minutes} = calculate()
+  horasElemento.textContent = hour
+  minutosElemento.textContent = minutes
 }
 
-// Função para exibir o modal quando chegar a hora de beber a próxima quantidade de água por timer
-function exibirModal() {
-  const modalContainer = document.querySelector(".modalContainer");
-  const modal = document.querySelector(".modal");
+updateHourAndMinutesDisplay()
 
-  // Exibir o modal
-  modalContainer.style.display = "flex";
-  modal.querySelector("span").textContent = "Lembrete para beber água!";
+// start counter 
+let isCounting = false, timer;
 
-  // Atualizar o contador
-  contador++;
+function countdown() {
+  clearInterval(timer)
+  if(!isCounting) return
 
-  // Verificar se já atingiu a meta diária
-  if (contador * quantidade >= metaDiaria) {
-    modal.querySelector("span").textContent = "Meta diária atingida!";
-    contador = 0;
+  const { canTakeWater } = calculate()
+  if(!canTakeWater) return
+  
+  let newHour = horasElemento.textContent,
+  newMinutes = Number(minutosElemento.textContent) - 1
+  
+  if(newMinutes < 0) {
+    newMinutes = 59
+    newHour = Number(horasElemento.textContent) - 1 
   }
+
+  if(newHour < 0) {
+    clearInterval(timer)
+    isCounting = false;
+    openModal()
+    return
+  }
+
+  horasElemento.textContent = newHour
+  minutosElemento.textContent = newMinutes
+
+  timer = setInterval(() => { countdown() }, 10)
 }
 
-// Chamar a função atualizarContador inicialmente e sempre que os inputs do tipo range mudarem
-atualizarContador();
-metaDiariaInput.addEventListener("input", function () {
-  metaDiaria = parseInt(metaDiariaInput.value);
-  atualizarContador();
-});
-quantidadeInput.addEventListener("input", function () {
-  quantidade = parseInt(quantidadeInput.value);
-  atualizarContador();
-});
+const startButton = document.getElementById('start')
+startButton.addEventListener('click', () => {
+  if(isCounting) return
+  
+  isCounting = true;
+  countdown()
+})
 
-// Obter o botão de iniciar
-const botaoIniciar = document.getElementById("start");
 
-// Função para iniciar o contador e exibir o modal quando chegar a hora de beber a próxima quantidade de água por timer
-function iniciarContador() {
-  setInterval(exibirModal, quantidade * 60 * 1000);
+// Modal
+const modalContainer = document.querySelector('.modalContainer')
+const closeButton = document.getElementById('close')
+
+closeButton.addEventListener('click', closeModal)
+
+function closeModal() {
+  modalContainer.style.display = 'none'
+  isCounting = true
+  updateHourAndMinutesDisplay()
+  countdown()
+  updatePercentageDisplay()
 }
 
-// Iniciar o contador quando o botão de iniciar for clicado
-botaoIniciar.addEventListener("click", iniciarContador);
+function openModal() {
+  isCounting = false;
+  modalContainer.style.display = 'block'
+}
+
+function updatePercentageDisplay() {
+  totalGlassesTaken++
+  const { percentageOfTotalGlassesTaken } = calculate()
+  
+  const percentageDisplay = document.getElementById('percentage')
+  percentageDisplay.textContent = percentageOfTotalGlassesTaken
+}
+updatePercentageDisplay()
